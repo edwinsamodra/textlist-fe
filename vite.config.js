3842dfd61224e22c4ede29b94ed0351e2c1b53c4
Edwin Samodra <edwinsp001@gmail.com>
2025-05-18 18:16:59 +0700

    update 0.1

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return defineConfig({
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      host: env.VITE_MYIPADDR,
+      port: parseInt(env.VITE_MYPORT),
+    },
+    define: {
+      'process.env': {}
+    }
+  })
+}
